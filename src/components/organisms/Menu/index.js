16263e7d1cbb8358815ad5795a4a3b01e4a06e23
unknown <julie.ramis1@gmail.com>
2021-02-15 05:50:57 +0100

    start menu

---
+++ src/components/organisms/Menu/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Menu = () => {
+    <>
+    </>
+
+
+return (
+    <div className="menu">
+        
+    </div>
+)
+}
+
+export default Menu
---
+++ src/views/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react"
+
+import Menu from "../components/organisms/Menu"
+
+import "../style/main.scss"
+
+const App = () => {
+
+  return (
+    <>
+    <Menu/>
+      <p>A</p>
+    </>
+  )
+
+}
+
+export default App;
