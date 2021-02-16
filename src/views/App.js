c8a78adbe3c14c39ae10a153192a34b1344df5a7
unknown <julie.ramis1@gmail.com>
2021-02-16 03:26:45 +0100

    list react-select

--- src/views/App.js
+++ src/views/App.js
@@ -3,12 +3,14 @@ import React, { useState } from "react"
 import Menu from "../components/organisms/Menu"
 
 import "../style/main.scss"
+import MyComponent from "../components/molecules/List"
 
 const App = () => {
 
   return (
     <>
     <Menu/>
+    <MyComponent/>
       <p>A</p>
     </>
   )
