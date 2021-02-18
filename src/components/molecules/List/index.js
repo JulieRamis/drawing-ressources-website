b45edfb24f247d22351727c1f36013f57d4731e7
unknown <julie.ramis1@gmail.com>
2021-02-18 11:47:51 +0100

    style react select

--- src/components/molecules/List/index.js
+++ src/components/molecules/List/index.js
@@ -2,13 +2,35 @@ import React, { Component } from 'react'
 import Select from 'react-select'
 
 const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
+  { value: 'posture', label: 'Postures' },
+  { value: 'cloth', label: 'Vêtements' },
+  { value: 'haircut', label: 'Coupe de cheveux' },
+  { value: 'anatomy', label: 'Anatomie' },
+  { value: 'expressions', label: 'Expressions' },
+  { value: 'creature', label: 'Créatures, Animaux' },
+  { value: 'sets', label: 'Décors' },
+  { value: 'materials', label: 'Matériaux' },
+  { value: 'perspective', label: 'Perspective' },
+  { value: 'shadow-light', label: 'Ombre, Lumière' },
+  { value: 'colorimetry', label: 'Colorimétrie' }
 ]
 
 const MyComponent = () => (
-  <Select options={options} />
+  
+  <Select onChange={() => console.log(options)} 
+    options={options}
+    defaultValue= {[options[5]]}
+    theme={(theme) => ({
+      ...theme,
+      borderRadius: 0,
+      colors: {
+      ...theme.colors,
+        text: 'orangered',
+        primary25: 'hotpink',
+        primary: '#a4243b',
+      },
+    })}
+  />
 )
 
 export default MyComponent
