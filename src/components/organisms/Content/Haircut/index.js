981eec8ed5d43e95e9bcbe0823c7cc9004488371
unknown <julie.ramis1@gmail.com>
2021-03-06 02:15:33 +0100

    pages perspective and haircut

--- src/components/organisms/Content/Haircut/index.js
+++ src/components/organisms/Content/Haircut/index.js
@@ -3,9 +3,99 @@ import React from 'react'
 
 
 const Haircut = () => (
-  <h2 className="telist">
-     Haircut
-  </h2>
+  <>
+    
+  <section className="clothes">
+  <article className="section-link-gallery blue-bc">
+    <h2>Ma sélection Pinterest</h2>
+  <div className="gallery">
+    <figure className="figure-portfolio">
+      <figcaption className="figcaption-gallery"><h3>Photos cheveux femmes</h3></figcaption>
+      <a href="https://www.pinterest.fr/julieramis1/coupes-de-cheveux-femmes/">
+        <div className="border">
+        <img src="img/haircut/hair-p-w-pinterest.png" alt="miniature-haircut-w-pinterest" className="img-gallery" />
+        <span className="span1"></span><span className="span2"></span>
+        </div>
+      </a>
+    </figure>
+
+    <figure className="figure-portfolio">
+      <figcaption className="figcaption-gallery"><h3>Photos cheveux hommes</h3></figcaption>
+      <a href="https://www.pinterest.fr/julieramis1/coupes-de-cheveux-hommes/">
+        <div className="border">
+        <img src="img/haircut/hair-p-m-pinterest.png" alt="miniature-haircut-m-pinterest" className="img-gallery" />
+        <span className="span1"></span><span className="span2"></span>
+        </div>
+      </a>
+      
+    </figure>
+
+    <figure className="figure-portfolio">
+      <figcaption className="figcaption-gallery"><h3>Dessins cheveux femmes</h3></figcaption>
+      <a href="https://www.pinterest.fr/julieramis1/cheveux-dessins-femmes/">
+        <div className="border">
+        <img src="img/haircut/hair-d-w-pinterest.png" alt="miniature-haircut-draw-w-pinterest" className="img-gallery" />
+        <span className="span1"></span><span className="span2"></span>
+        </div>
+      </a>
+    </figure>
+
+    <figure className="figure-portfolio">
+      <figcaption className="figcaption-gallery"><h3>Dessins cheveux hommes</h3></figcaption>
+      <a href="https://www.pinterest.fr/julieramis1/cheveux-dessins-hommes/">
+        <div className="border">
+        <img src="img/haircut/hair-d-m-pinterest.png" alt="miniature-haircut-draw-m-pinterest" className="img-gallery" />
+        <span className="span1"></span><span className="span2"></span>
+        </div>
+      </a>
+    </figure>
+  </div> 
+</article>
+<article className="section-link-gallery yt">
+<h2>Ma sélection Youtube</h2>
+<div className="gallery">
+  <iframe width="410" height="229" src="https://www.youtube.com/embed/Uv77x3926_k" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    
+  <iframe width="410" height="229" src="https://www.youtube.com/embed/sK75zntjgXA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe width="410" height="229" src="https://www.youtube.com/embed/8qic8vmycEs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  
+</div>
+</article>
+
+<article className="section-link-gallery blue-bc">
+<h2>Ma sélection de sites</h2>
+<div className="gallery">
+  <figure className="figure-portfolio">
+    <figcaption className="figcaption-gallery"><h3>Conseils pour dessiner les cheveux</h3></figcaption>
+    <a href="https://www.apprendre-a-dessiner.org/dessin-cheveux/">
+      <div className="border">
+      <img src="img/haircut/hair-site.png" alt="miniature-hair-site" className="img-gallery" />
+      <span className="span1"></span><span className="span2"></span></div>
+    </a>
+  </figure>
+
+  <figure className="figure-portfolio">
+    <figcaption className="figcaption-gallery"><h3>Tuto cheveux façon manga</h3></figcaption>
+    <a href="https://inarisensei.blogspot.com/2018/09/tuto-cheveux-basiques-manga.html">
+      <div className="border">
+      <img src="img/haircut/hair-site-2.png" alt="miniature-hair-site-2" className="img-gallery" />
+      <span className="span1"></span><span className="span2"></span>
+      </div>
+    </a>
+    
+  </figure>
+
+  <figure className="figure-portfolio">
+    <figcaption className="figcaption-gallery"><h3>Guide pour dessiner les cheveux</h3></figcaption>
+    <a href="https://www.clipstudio.net/comment-dessiner/archives/159956">
+      <div className="border">
+      <img src="img/haircut/hair-site-3.png" alt="miniature-hair-site-3" className="img-gallery" />
+      <span className="span1"></span><span className="span2"></span>
+      </div>
+    </a>
+  </figure>
+    </div>
+</article>
+</section>
+</>
 )
 
 export default Haircut
--- src/components/organisms/Content/Perspective/index.js
+++ src/components/organisms/Content/Perspective/index.js
@@ -3,9 +3,99 @@ import React from 'react'
 
 
 const Perspective = () => (
-  <h2 className="telist">
-     Perspective
-  </h2>
+  <>
+    
+  <section className="clothes">
+  <article className="section-link-gallery blue-bc">
+    <h2>Ma sélection Pinterest</h2>
+  <div className="gallery">
+    <figure className="figure-portfolio">
+      <figcaption className="figcaption-gallery"><h3>Photos perspective corps</h3></figcaption>
+      <a href="https://www.pinterest.fr/julieramis1/coupes-de-cheveux-femmes/">
+        <div className="border">
+        <img src="img/perspective/perspective-p-c-pinterest.png" alt="miniature-haircut-w-pinterest" className="img-gallery" />
+        <span className="span1"></span><span className="span2"></span>
+        </div>
+      </a>
+    </figure>
+
+    <figure className="figure-portfolio">
+      <figcaption className="figcaption-gallery"><h3>Photos perspective paysages</h3></figcaption>
+      <a href="https://www.pinterest.fr/julieramis1/coupes-de-cheveux-hommes/">
+        <div className="border">
+        <img src="img/perspective/perspective-p-p-pinterest.png" alt="miniature-haircut-m-pinterest" className="img-gallery" />
+        <span className="span1"></span><span className="span2"></span>
+        </div>
+      </a>
+      
+    </figure>
+
+    <figure className="figure-portfolio">
+      <figcaption className="figcaption-gallery"><h3>Dessins perspective corps</h3></figcaption>
+      <a href="https://www.pinterest.fr/julieramis1/cheveux-dessins-femmes/">
+        <div className="border">
+        <img src="img/perspective/perspective-d-c-pinterest.png" alt="miniature-haircut-draw-w-pinterest" className="img-gallery" />
+        <span className="span1"></span><span className="span2"></span>
+        </div>
+      </a>
+    </figure>
+
+    <figure className="figure-portfolio">
+      <figcaption className="figcaption-gallery"><h3>Dessins perspective paysage</h3></figcaption>
+      <a href="https://www.pinterest.fr/julieramis1/cheveux-dessins-hommes/">
+        <div className="border">
+        <img src="img/perspective/perspective-d-p-pinterest.png" alt="miniature-haircut-draw-m-pinterest" className="img-gallery" />
+        <span className="span1"></span><span className="span2"></span>
+        </div>
+      </a>
+    </figure>
+  </div> 
+</article>
+<article className="section-link-gallery yt">
+<h2>Ma sélection Youtube</h2>
+<div className="gallery">
+  <iframe width="410" height="229" src="https://www.youtube.com/embed/f6112Frz13A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    
+  <iframe width="410" height="229" src="https://www.youtube.com/embed/Vl9NWhjVSy0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  <iframe width="410" height="229" src="https://www.youtube.com/embed/b7fQ3627rvk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>  
+</div>
+</article>
+
+<article className="section-link-gallery blue-bc">
+<h2>Ma sélection de sites</h2>
+<div className="gallery">
+  <figure className="figure-portfolio">
+    <figcaption className="figcaption-gallery"><h3>Règles pour dessiner en perspective</h3></figcaption>
+    <a href="http://dciner.fr/les-5-regles-pour-bien-dessiner-en-perspective/">
+      <div className="border">
+      <img src="img/perspective/perspective-site.png" alt="miniature-perspective-site" className="img-gallery" />
+      <span className="span1"></span><span className="span2"></span></div>
+    </a>
+  </figure>
+
+  <figure className="figure-portfolio">
+    <figcaption className="figcaption-gallery"><h3>Les secrets de la perspective</h3></figcaption>
+    <a href="https://www.lateliercanson.com/les-secrets-de-la-perspective">
+      <div className="border">
+      <img src="img/perspective/perspective-site-2.png" alt="miniature-perspective-site-2" className="img-gallery" />
+      <span className="span1"></span><span className="span2"></span>
+      </div>
+    </a>
+    
+  </figure>
+
+  <figure className="figure-portfolio">
+    <figcaption className="figcaption-gallery"><h3>Comment mesurer la perspective</h3></figcaption>
+    <a href="https://www.apprendre-a-dessiner.org/dessin-perspective/">
+      <div className="border">
+      <img src="img/perspective/perspective-site-3.png" alt="miniature-perspective-site-3" className="img-gallery" />
+      <span className="span1"></span><span className="span2"></span>
+      </div>
+    </a>
+  </figure>
+    </div>
+</article>
+</section>
+</>
 )
 
 export default Perspective
