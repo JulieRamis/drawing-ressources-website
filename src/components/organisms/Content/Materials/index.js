6a1905201ae5f3302131f10cd442179f21a09187
unknown <julie.ramis1@gmail.com>
2021-03-12 02:03:25 +0100

    Materials page

--- src/components/organisms/Content/Materials/index.js
+++ src/components/organisms/Content/Materials/index.js
@@ -3,9 +3,130 @@ import React from 'react'
 
 
 const Materials = () => (
-  <h2 className="telist">
-     Materials
-  </h2>
+  <>
+    <section className="clothes">
+      <article className="section-link-gallery blue-bc">
+        <h2>Ma sélection Pinterest</h2>
+        <div className="gallery">
+          <figure className="figure-portfolio">
+
+            <figcaption className="figcaption-gallery"><h3>Pierres photos</h3></figcaption>
+            <a href="https://www.pinterest.fr/julierda1/pierres-photos/">
+              <div className="border">
+                <img src="img/materials/pierres-p-pinterest.png" alt="miniature-materials-w-pinterest" className="img-gallery" />
+                <span className="span1"></span><span className="span2"></span>
+              </div>
+            </a>
+          </figure>
+
+          <figure className="figure-portfolio">
+
+            <figcaption className="figcaption-gallery"><h3>Pierres dessins</h3></figcaption>
+            <a href="https://www.pinterest.fr/julierda1/pierres/">
+              <div className="border">
+                <img src="img/materials/pierres-d-pinterest.png" alt="miniature-materials-w-pinterest" className="img-gallery" />
+                <span className="span1"></span><span className="span2"></span>
+              </div>
+            </a>
+          </figure>
+          <figure className="figure-portfolio">
+
+            <figcaption className="figcaption-gallery"><h3>Tissus photos</h3></figcaption>
+            <a href="https://www.pinterest.fr/julierda1/tissu/">
+              <div className="border">
+                <img src="img/materials/tissus-p-pinterest.png" alt="miniature-materials-w-pinterest" className="img-gallery" />
+                <span className="span1"></span><span className="span2"></span>
+              </div>
+            </a>
+          </figure>
+          <figure className="figure-portfolio">
+
+            <figcaption className="figcaption-gallery"><h3>Tissus dessins</h3></figcaption>
+            <a href="https://www.pinterest.fr/julierda1/tissu-dessin/">
+              <div className="border">
+                <img src="img/materials/tissus-d-pinterest.png" alt="miniature-materials-w-pinterest" className="img-gallery" />
+                <span className="span1"></span><span className="span2"></span>
+              </div>
+            </a>
+          </figure>
+
+          <figure className="figure-portfolio">
+
+            <figcaption className="figcaption-gallery"><h3>Dentelles photos</h3></figcaption>
+            <a href="https://www.pinterest.fr/julierda1/dentelles-photos/">
+              <div className="border">
+                <img src="img/materials/dentelle-p-pinterest.png" alt="miniature-materials-w-pinterest" className="img-gallery" />
+                <span className="span1"></span><span className="span2"></span>
+              </div>
+            </a>
+          </figure>
+          <figure className="figure-portfolio">
+
+            <figcaption className="figcaption-gallery"><h3>Dentelles dessins</h3></figcaption>
+            <a href="https://www.pinterest.fr/julierda1/dentelle-dessins/">
+              <div className="border">
+                <img src="img/materials/dentelle-d-pinterest.png" alt="miniature-materials-w-pinterest" className="img-gallery" />
+                <span className="span1"></span><span className="span2"></span>
+              </div>
+            </a>
+          </figure>
+
+
+
+
+        </div>
+      </article>
+      <article className="section-link-gallery yt">
+        <h2>Ma sélection Youtube</h2>
+        <div className="gallery">
+        <iframe width="410" height="229" src="https://www.youtube.com/embed/4KrrZ6eH3uo" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="410" height="229" src="https://www.youtube.com/embed/FMm2MF3B2Js" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="410" height="229" src="https://www.youtube.com/embed/cWwK8TUI3Ac" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="410" height="229" src="https://www.youtube.com/embed/dk68NwMfi2k" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="410" height="229" src="https://www.youtube.com/embed/tXYy7p_HHcY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="410" height="229" src="https://www.youtube.com/embed/Z7MsvJz82u0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe width="410" height="229" src="https://www.youtube.com/embed/unb_0KaavZA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        </div>
+      </article>
+
+      <article className="section-link-gallery blue-bc">
+        <h2>Ma sélection de sites</h2>
+        <div className="gallery">
+          <figure className="figure-portfolio">
+            <figcaption className="figcaption-gallery"><h3>Dessiner une pierre aux crayons</h3></figcaption>
+            <a href="http://lessfr.jesima.com/article/345">
+              <div className="border">
+                <img src="img/materials/materials-site.png" alt="miniature-materials-site" className="img-gallery" />
+                <span className="span1"></span><span className="span2"></span></div>
+            </a>
+          </figure>
+
+          <figure className="figure-portfolio">
+            <figcaption className="figcaption-gallery"><h3>Dessiner des rochers en 5 min</h3></figcaption>
+            <a href="http://toutdessiner.com/2019/04/01/comment-dessiner-des-rochers-en-5-min/">
+              <div className="border">
+                <img src="img/materials/materials-site-2.png" alt="miniature-materials-site-2" className="img-gallery" />
+                <span className="span1"></span><span className="span2"></span>
+              </div>
+            </a>
+
+          </figure>
+
+          <figure className="figure-portfolio">
+            <figcaption className="figcaption-gallery"><h3>Dessiner du tissu</h3></figcaption>
+            <a href="https://fr.accentsconagua.com/articles/design/how-to-draw-fabric-silk-and-wool.html">
+              <div className="border">
+                <img src="img/materials/materials-site-3.png" alt="miniature-materials-site-3" className="img-gallery" />
+                <span className="span1"></span><span className="span2"></span>
+              </div>
+            </a>
+          </figure>
+
+        </div>
+      </article>
+    </section>
+  </>
 )
 
 export default Materials
